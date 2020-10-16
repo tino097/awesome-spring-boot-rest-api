ba634b827b7137e5b1ad989f3734496888d799f1
xtreme-uz <rustambekov.mail.box@gmail.com>
2020-10-16 11:46:09 +0500

    fix: some bugs

--- src/main/java/com/sivakov/Application.java
+++ src/main/java/com/sivakov/Application.java
@@ -4,15 +4,10 @@ package com.sivakov;
  *
  */
 
-import com.sivakov.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@Import({SwaggerConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
